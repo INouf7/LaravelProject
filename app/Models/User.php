b862924e5abfff9c402629bdabef254c11a93a4b
nouf <nouf@gmail.com>
2021-12-18 21:40:34 -0500

    removed the personal team and provided option to create a team after signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function isMemberOfATeam(){
+        return $this->allTeams()->count()>0;
+    }
 }
